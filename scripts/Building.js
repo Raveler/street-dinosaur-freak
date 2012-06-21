38c668a58b6fc9364a998e9f7b752ba666361e92
dave <dave>
2012-06-21 19:30:55 +0200

    Crappy code of Elliot added

--- scripts/Building.js
+++ scripts/Building.js
@@ -23,10 +23,18 @@ define(["Compose", "Logger", "Background", "Random", "Vector2"], function(Compos
 	{
 
 		draw: function(ctx, worldPosition) {
+			ctx.save();
+			ctx.translate(-worldPosition, 0);
+			//ctx.scale(-1, 1);
+			//if (random.getInt(0, 1) == 0){
+			//	ctx.scale(-1, 1);
+			//	ctx.translate(this.buildBlockWidth, 0);
+			//}
 			for(var i = 0; i < this.buildingBlocks.length; i++) {
-				ctx.drawImage(this.game.images[this.buildingBlocks[i]], this.position - worldPosition, this.game.height - (this.buildBlockHeight * (i + 1)));
+				ctx.drawImage(this.game.images[this.buildingBlocks[i]], this.position, this.game.height - (this.buildBlockHeight * (i + 1)));
 			}
-			ctx.drawImage(this.game.images[this.buildingTop], this.position - worldPosition, this.game.height - (this.buildBlockHeight * (i + 1)));
+			ctx.drawImage(this.game.images[this.buildingTop], this.position, this.game.height - (this.buildBlockHeight * (i + 1)));
+			ctx.restore();
 		},
 
 		checkCollision: function(ctx, point) {
